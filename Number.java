fa40443fbe8f8d413e8f95ce2ccd8539513e5182
Dunni-Kanye <dunniade18@gmail.com>
2024-06-21 11:15:43 -0700

    My Assignment

---
+++ Number.java
@@ -0,0 +1,13 @@
+public class Number {
+
+public static void main(String[]args) {
+
+int numbers = 3000;
+
+String message = " i am a girl ";
+ 
+
+System.out.printf("%s%d%f%c%b" girl 2.8d, 5.8f, A, true);
+  }
+
+}
